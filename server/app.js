2979cfe753c4edaddc4abf446c66508bcdcab2fd
ShuhbamUpadhyayDeveloper <shubhamwork48@gmail.com>
2024-04-01 12:57:22 +0530

    deployment changes

--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@ const port = process.env.PORT || 4000
 const connectToMongodb  = require('./utils/mongodb.connect')
 const usersRoute = require('./routes/users.route')
 const teamRoute = require('./routes/teams.route')
-const cloudinary = require('cloudinary').v2
 
 // middlewares
 app.use(express.json())
@@ -17,12 +16,6 @@ app.get('/', (req ,res) => {
     res.send("Hi welcome to chat app")
 })
 
-// cloudinary config
-cloudinary.config({
-    cloud_name : process.env.CLOUD_NAME,
-    api_key : process.env.CLOUD_KEY,
-    api_secret : process.env.CLOUD_SECRET
-})
  
 app.listen(port,() => {
     connectToMongodb()
--- server/routes/users.route.js
+++ server/routes/users.route.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const router =  express.Router()
 const {getUsers,getSpecificUser,createUser,updateUser,deleteUser} = require('../controllers/users.controler')
-const {storage} = require('../utils/storage')
-const multer = require('multer')
-const upload = multer({storage})
 router.get("/" , getUsers)
 router.get('/:userId',getSpecificUser)
 router.post('/', createUser)
-router.put('/:userId', upload.single("avatar") ,updateUser)
+router.put('/:userId',updateUser)
 router.delete('/:userId',deleteUser)
 
 module.exports = router;
